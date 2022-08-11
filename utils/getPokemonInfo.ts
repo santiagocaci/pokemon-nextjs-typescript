b3015920357aaf179b0048870e1c4d5e2b11ac5b
Santiago Caci <santiago.caci@gmail.com>
2022-08-10 21:39:21 -0300

    Fix a pokemon response

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,10 +3,21 @@ import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
   const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+  const { id, name, sprites } = data;
 
   return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
+    id,
+    name,
+    sprites: {
+      other: {
+        dream_world: {
+          front_default: sprites.other?.dream_world.front_default,
+        },
+      },
+      front_default: sprites.front_default,
+      back_default: sprites.back_default,
+      front_shiny: sprites.front_shiny,
+      back_shiny: sprites.back_shiny,
+    },
   };
 };
